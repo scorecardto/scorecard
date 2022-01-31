000368a07c6e0b28f39b6311771a471e41d6eeac
Adrian Casares <67245238+adriancasares@users.noreply.github.com>
2022-01-30 21:56:14 -0600

    col resizing

--- src/components/interactive/TableColumn.tsx
+++ src/components/interactive/TableColumn.tsx
@@ -1,53 +1,79 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 type TableColumnProps = {
   cells: Element[];
 };
 
 export default function TableColumn({ cells }: TableColumnProps) {
-  const [width, setWidth] = useState(100);
+  const [width, setWidth] = useState<number | undefined>(undefined);
+  const [resizing, setResizing] = useState(false);
+  const [originalWidth, setOriginalWidth] = useState(0);
+  const [defaultWidth, setDefaultWidth] = useState(0);
 
-  const resizeMouseDown = (e) => {
+  const resizeMouseDown = (e: any) => {
+    e.preventDefault();
+
     const startX = e.clientX;
     const startWidth = parseInt(
       document?.defaultView?.getComputedStyle(e.target)?.width ?? '',
       10
     );
 
-    const mouseMove = (e2) => {
-      setWidth(startWidth + e2.clientX - startX);
+    const mouseMove = (e2: any) => {
+      setWidth(
+        Math.max(
+          originalWidth + 1,
+          defaultWidth + startWidth + e2.clientX - startX
+        )
+      );
     };
 
     const mouseUp = () => {
+      setResizing(false);
       document.documentElement.removeEventListener('mousemove', mouseMove);
       document.documentElement.removeEventListener('mouseup', mouseUp);
-      // document.documentElement.addEventListener('mousedown', mouseUp);
     };
 
     document.documentElement.addEventListener('mousemove', mouseMove);
     document.documentElement.addEventListener('mouseup', mouseUp);
-    // document.documentElement.addEventListener('mousedown', mouseUp);
+
+    setResizing(true);
   };
+
+  const col = React.createRef<HTMLDivElement>();
+
+  useEffect(() => {
+    setOriginalWidth(col?.current?.clientWidth ?? 0);
+  }, []);
+
+  useEffect(() => {
+    setDefaultWidth(col?.current?.clientWidth ?? 0);
+  }, [col]);
 
   return (
     <>
-      <div style={{ width }}>
+      <div style={{ width }} ref={col}>
         {cells.map((cell, idx) => {
           return (
             <p
-              className="block pl-6 py-2 pr-2 border-b border-b-gray-400 last:border-none text-base"
+              className="block text-base relative border-b border-b-gray-400 last:border-none"
               key={idx}
             >
-              {cell}
+              <span className="pl-6 py-2 pr-2 inline-block ">{cell}</span>
+              <span
+                className="w-1 h-full absolute right-1 cursor-col-resize"
+                onMouseDown={resizeMouseDown}
+              ></span>
+              <span
+                className={`w-1 h-full absolute right-0 ${
+                  resizing ? 'border-l-2' : 'border-l'
+                } border-l-gray-400 cursor-col-resize`}
+                onMouseDown={resizeMouseDown}
+              ></span>
             </p>
           );
         })}
       </div>
-
-      <div
-        className="border-r-8 border-r-blue-400"
-        onMouseDown={resizeMouseDown}
-      ></div>
     </>
   );
 }
