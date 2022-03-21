921ba7a0cd39609df09a7ad4559fa4916dd4090c
Adrian Casares <67245238+adriancasares@users.noreply.github.com>
2022-03-21 08:54:33 -0500

    fix spacing on textcard without icon

--- src/components/card/TextCard.tsx
+++ src/components/card/TextCard.tsx
@@ -14,7 +14,7 @@ export default function TextCard({ icon, children, onClick }: Props) {
       className="_text-card hover:bg-day-600 hover:dark:bg-night-600 text-day-500 dark:text-night-500 w-fit px-3 py-1 rounded-md transition-colors cursor-pointer flex items-center"
       onClick={onClick}
     >
-      {<span className="mr-2">{icon}</span> ?? <></>}
+      {icon != null ? <span className="mr-2">{icon}</span> : <></>}
       <p>{children}</p>
     </div>
   );
