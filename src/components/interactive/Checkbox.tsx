a2a270721a84a4fd16f19a3c1796563f9fdf23b5
Adrian Casares <67245238+adriancasares@users.noreply.github.com>
2022-04-17 20:46:21 -0500

    friendlier checkbox UI

--- src/components/interactive/Checkbox.tsx
+++ src/components/interactive/Checkbox.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import { IoCheckmarkSharp } from 'react-icons/io5';
 
@@ -15,21 +15,8 @@ export default function Checkbox({
   onClick,
   cancelEvent,
 }: ICheckboxProps) {
-  const [mouseDown, setMouseDown] = useState(false);
   return (
     <div
-      onMouseDown={(e) => {
-        if (!editingEnabled || e.button !== 0) return;
-
-        setMouseDown(true);
-
-        const upCallback = () => {
-          setMouseDown(false);
-          document.removeEventListener('mouseup', upCallback);
-        };
-
-        document.addEventListener('mouseup', upCallback);
-      }}
       onClick={(e) => {
         if (editingEnabled) onClick(!checked);
         if (cancelEvent) {
@@ -37,24 +24,21 @@ export default function Checkbox({
           e.stopPropagation();
         }
       }}
-      className={`text-day-100 w-5 h-5 flex items-center justify-center rounded-sm text-sm transition-background-transform duration-200 ${(() => {
-        if (!checked) {
-          return 'border border-day-400 dark:border-night-400';
-        }
-        if (editingEnabled) {
-          return 'bg-theme-200';
-        }
-        return 'bg-day-500 dark:bg-night-500';
-      })()} ${mouseDown ? 'scale-75' : ''}`}
     >
-      <div>
-        <IoCheckmarkSharp
-          className={
-            checked
-              ? 'opacity-100 transition-colors'
-              : 'opacity-0 transition-colors'
+      <div
+        className={`text-day-100 w-5 h-5 flex items-center justify-center rounded-sm text-sm my-4 ${(() => {
+          if (!checked) {
+            return 'border border-day-400 dark:border-night-400';
           }
-        />
+          if (editingEnabled) {
+            return 'bg-theme-200';
+          }
+          return 'bg-day-500 dark:bg-night-500';
+        })()}`}
+      >
+        <div>
+          <IoCheckmarkSharp className={checked ? 'opacity-100' : 'opacity-0'} />
+        </div>
       </div>
     </div>
   );
