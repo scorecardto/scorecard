b94b08c94dea4d93f50b8b328ff4b3e71b1561b0
Adrian Casares <67245238+adriancasares@users.noreply.github.com>
2022-04-17 19:00:17 -0500

    category styling

--- src/components/testing/TCategory.tsx
+++ src/components/testing/TCategory.tsx
@@ -33,7 +33,10 @@ export default function TCategory({
   }, [addedAssignments]);
 
   return (
-    <div className="_TCategory" key={categoryIdx}>
+    <div
+      className="_TCategory border-b border-day-300 dark:border-night-300 pb-2 last:border-b-0"
+      key={categoryIdx}
+    >
       <div className="_TCategory-name h-12 flex items-center text-day-400 dark:text-night-400 pl-12 pr-4 justify-between">
         <p>{category.category.name}</p>
         <label className="flex items-center gap-1 bg-theme-100 text-theme-200 py-1 px-2 rounded-md text-sm">
