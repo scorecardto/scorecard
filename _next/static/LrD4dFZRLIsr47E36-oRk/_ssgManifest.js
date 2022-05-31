7ffbc295ccc00d5e8da0587316be7a38075448b5
adriancasares <67245238+adriancasares@users.noreply.github.com>
2022-05-31 19:03:05 +0000

    Deploying to gh-pages from  @ 47c9cb237ed3a9981c579382174f0f79b40d9e82 🚀

---
+++ _next/static/LrD4dFZRLIsr47E36-oRk/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fassignments","\u002F","\u002Finsights\u002Fnotifications","\u002Finsights\u002Ftesting","\u002Finsights\u002Fgpa"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
