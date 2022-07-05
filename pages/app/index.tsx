54b2f8c665d5c56dbfe841ed0adbd9f70cad42ff
Adrian Casares <67245238+adriancasares@users.noreply.github.com>
2022-07-05 18:40:50 -0500

    add runtime send message

---
+++ pages/app/index.tsx
@@ -0,0 +1,23 @@
+import type { NextPage } from "next";
+import { useState } from "react";
+
+const App: NextPage = () => {
+  const [data, setData] = useState(undefined);
+
+  chrome.runtime
+    .sendMessage("fkpgodekaimcnfknnkgkkdclfodblifl", {
+      message: "hello world!",
+    })
+    .then((response) => {
+      setData(response);
+    });
+
+  return (
+    <div>
+      {!data && <p>Loading...</p>}
+      {data && <p>{data}</p>}
+    </div>
+  );
+};
+
+export default App;
