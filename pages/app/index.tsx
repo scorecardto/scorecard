167894b89d8fd19739f7e42528035aae903ace19
Adrian Casares <67245238+adriancasares@users.noreply.github.com>
2022-07-05 19:30:47 -0500

    use AppLoadState to handle loading

--- pages/app/index.tsx
+++ pages/app/index.tsx
@@ -1,31 +1,43 @@
 import type { NextPage } from "next";
 import { useState } from "react";
 
+type AppLoadState =
+  | "LOADING"
+  | "DONE"
+  | "ERR_BROWSER"
+  | "ERR_EXT_NOT_INSTALLED"
+  | "ERR_EXT_VERSION";
+
 const App: NextPage = () => {
   const [data, setData] = useState(undefined);
 
-  if (typeof window !== "undefined") {
-    try {
-      chrome.runtime.sendMessage(
-        "fkpgodekaimcnfknnkgkkdclfodblifl",
-        {
-          message: "hello world!",
-        },
-        (response) => {
-          console.log("got a response", response);
+  const [loadState, setLoadState] = useState<AppLoadState>("LOADING");
 
-          setData(response);
+  if (typeof window !== "undefined") {
+    if (window["chrome"] && chrome.runtime?.["sendMessage"]) {
+      chrome.runtime.sendMessage({ type: "getData" }, (response) => {
+        if (response.version === "0.1") {
+          setData(response.data);
+          setLoadState("DONE");
+        } else {
+          setLoadState("ERR_EXT_VERSION");
         }
-      );
-    } catch (e) {
-      console.log(e);
+      });
+    } else {
+      setLoadState("ERR_BROWSER");
     }
   }
 
   return (
     <div>
-      {!data && <p>Loading...</p>}
-      {data && <p>{data}</p>}
+      {loadState === "LOADING" && <p>Loading...</p>}
+      {loadState === "ERR_BROWSER" && (
+        <p>Sorry, your browser is not compatible.</p>
+      )}
+      {loadState === "ERR_EXT_NOT_INSTALLED" && (
+        <p>Please install Scorecard to continue.</p>
+      )}
+      {loadState === "ERR_EXT_VERSION" && <p>Your Scorecard is outdated.</p>}
     </div>
   );
 };
