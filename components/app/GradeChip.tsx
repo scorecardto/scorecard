6e0607ca06c57e06881351d131756470a0865b88
Adrian Casares <67245238+adriancasares@users.noreply.github.com>
2022-10-07 09:34:09 -0500

    grade modesty 😀

--- components/app/GradeChip.tsx
+++ components/app/GradeChip.tsx
@@ -1,7 +1,12 @@
 export default function GradeChip(props: { children?: any; faded?: boolean }) {
   return (
-    <div className="flex-none from-accent-400 to-accent-500 bg-gradient-to-tr rounded-xl pt-2.5 pb-1.5 px-3">
-      <p className="align-middle text-white font-mono">{props.children}</p>
+    <div className="flex-none from-accent-400 to-accent-500 bg-gradient-to-tr rounded-xl pt-2.5 pb-1.5 px-3 group">
+      <p className="align-middle text-white font-mono group-hover:hidden">
+        {"😀"}
+      </p>
+      <p className="align-middle text-white font-mono hidden group-hover:block">
+        {props.children}
+      </p>
     </div>
   );
 }
