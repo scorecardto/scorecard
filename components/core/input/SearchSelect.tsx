eae5c4e1976d18a55ee34a08c9e6e62aa382a116
Adrian Casares <67245238+adriancasares@users.noreply.github.com>
2022-10-08 23:00:16 -0500

    add district list

--- components/core/input/SearchSelect.tsx
+++ components/core/input/SearchSelect.tsx
@@ -1,9 +1,13 @@
 import React, { useState } from "react";
 import { IoSearchOutline } from "react-icons/io5";
 import TextInput from "./TextInput";
+import axios from "axios";
 
 export default function SearchSelect() {
   const [search, setSearch] = useState("");
+
+  // useMemo(() => , [second])
+
   return (
     <div>
       <TextInput
---
+++ pages/api/districts.tsx
@@ -0,0 +1,17 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import districtsJson from "../../lib/districts.json";
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  const districts = districtsJson.data.districts.map((district) => {
+    const url =
+      district.accounts["student"].url || district.accounts["Student"]?.url;
+    return {
+      name: district.name,
+      url: url ? new URL(url).hostname : null,
+    };
+  });
+  res.status(200).json({
+    lastUpdated: districtsJson.LAST_UPDATED,
+    districts,
+  });
+}
