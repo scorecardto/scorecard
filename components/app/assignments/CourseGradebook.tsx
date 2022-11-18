2f6beacec97d0203800682628eb8035382cb2a40
MaliciousFiles <mtroalson@gmail.com>
2022-11-18 09:35:11 -0600

    add reset button to name

--- components/app/assignments/CourseGradebook.tsx
+++ components/app/assignments/CourseGradebook.tsx
@@ -1,5 +1,5 @@
 import React, {useContext, useEffect, useLayoutEffect, useRef} from "react";
-import { FiEdit2 } from "react-icons/fi";
+import { FiEdit2, FiRotateCw } from "react-icons/fi";
 import { Course, DataContext } from "scorecard-types";
 import ActionChip from "../ActionChip";
 import GradeChip from "../GradeChip";
@@ -38,42 +38,53 @@ export default function CourseGradebook(props: { course: Course }) {
   let saveName = true;
 
   function updateName(evt: React.KeyboardEvent<HTMLHeadingElement>) {
-    if ((evt.key === "Enter" || evt.key === "Escape") && evt.target) {
+    if ((evt.key === "Enter" || evt.key === "Escape") && evt.currentTarget) {
       evt.bubbles = false;
 
       saveName = evt.key !== "Escape";
-      (evt.target as HTMLHeadingElement).blur();
+      evt.currentTarget.blur();
     }
   }
 
   function escapeName(evt: React.FocusEvent<HTMLHeadingElement>) {
-    if (evt.target) {
+    if (evt.currentTarget) {
       if (saveName) {
+        const newName = evt.currentTarget.innerText.trim();
+
         port?.postMessage({
           type: "updateCourseDisplayName",
           courseKey: course.key,
-          displayName: (evt.target as HTMLHeadingElement).innerText,
+          displayName: newName ? newName : course.name,
         });
       } else {
-        (evt.target as HTMLHeadingElement).innerText = (data.courseDisplayNames[course.key] || course.name);
+        evt.currentTarget.innerText = (data.courseDisplayNames[course.key] || course.name);
       }
 
-      (evt.target as HTMLHeadingElement).contentEditable = "false";
+      evt.currentTarget.contentEditable = "false";
     }
   }
 
   function makeEditable(evt: React.MouseEvent<HTMLHeadingElement>) {
-    if (evt.target) {
-      (evt.target as HTMLHeadingElement).contentEditable = "true";
-      (evt.target as HTMLHeadingElement).focus();
+    if (evt.currentTarget) {
+      evt.currentTarget.contentEditable = "true";
+      evt.currentTarget.focus();
     }
+  }
+
+  function reset(evt: React.MouseEvent<HTMLButtonElement>) {
+    port?.postMessage({
+      type: "updateCourseDisplayName",
+      courseKey: course.key,
+      displayName: course.name,
+    });
   }
 
   return (
     <motion.div className="flex flex-col gap-4">
       <div className="flex justify-between pl-12 pr-4 pt-8 pb-4">
         <div className="flex flex-col gap-2">
-          <div className="flex gap-2 items-center">
+          <div className="flex gap-2 items-center relative">
+            <button onClick={reset} className="text-mono-l-500 absolute right-full mr-1 hover:bg-slate-100 rounded-md p-1"><FiRotateCw/></button>
             <h1 className="text-3xl outline-0 decoration-3 transition-colors duration-300 decoration-transparent focus:decoration-blue-300 focus:underline" onClick={makeEditable} onKeyDown={updateName} onBlur={escapeName}>{data.courseDisplayNames[course.key] ?? course.name}</h1>
             <FiEdit2 className="text-mono-l-500" />
           </div>
