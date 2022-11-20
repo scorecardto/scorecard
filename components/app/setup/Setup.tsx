af518457d6fcd59ab5f1a738e65145a1796b414c
Adrian Casares <67245238+adriancasares@users.noreply.github.com>
2022-11-20 12:13:39 -0600

    improve setup page QOL

--- components/app/setup/Setup.tsx
+++ components/app/setup/Setup.tsx
@@ -94,111 +94,116 @@ export default function Setup(props: {
     return undefined;
   })();
 
+  function submit() {
+    if (!valid) return;
+
+    setChecking(true);
+    assert(district != null);
+
+    props
+      .checkSetup(district, username, changePassword ? password : undefined)
+      .then((result) => {
+        if (result === "VALID") {
+          router.push("/app");
+          return;
+        }
+
+        setChecking(false);
+
+        if (result === "INCORRECT_PASSWORD") {
+          setIncorrectPassword(true);
+        }
+
+        if (result === "INCORRECT_USERNAME") {
+          setIncorrectUsername(true);
+        }
+      });
+  }
+
   return (
     <FormPage
       title="Connect to Frontline"
       description="Enter your student account details to continue. Your Frontline login is not stored online, and your grades will not be visible to Scorecard."
     >
-      <div className="max-w-md flex flex-col gap-4 mx-auto">
-        {error && (
-          <div className="bg-red-100 border border-red-400 rounded-md flex gap-4 items-center py-2 px-4">
-            <IoAlertCircle className="text-red-400 text-xl" />
-            <p className="text-red-400">{error}</p>
-          </div>
-        )}
-        <DistrictSearch value={district} setValue={setDistrict} />
-        <TextInput
-          value={username}
-          setValue={setUsername}
-          error={incorrectUsername}
-          label="Username"
-          placeholder="Enter your frontline username"
-        />
-        {changePassword ? (
-          <div className="flex flex-row w-full items-end gap-2">
-            <div className="w-full">
-              <TextInput
-                value={password}
-                setValue={setPassword}
-                password={true}
-                error={incorrectPassword}
-                label="Password"
-                placeholder="Your password will not be stored online"
-              />
+      <form
+        onSubmit={(e) => {
+          e.stopPropagation();
+          e.preventDefault();
+        }}
+      >
+        <div className="max-w-md flex flex-col gap-4 mx-auto">
+          {error && (
+            <div className="bg-red-100 border border-red-400 rounded-md flex gap-4 items-center py-2 px-4">
+              <IoAlertCircle className="text-red-400 text-xl" />
+              <p className="text-red-400">{error}</p>
             </div>
-            {new URLSearchParams(window.location.search).get(
-              "changePassword"
-            ) == "false" && (
-              <div
-                className="h-10 w-10 flex items-center justify-center bg-accent-150 dark:bg-accent-650 border border-accent-250 dark:border-accent-650 rounded-md text-accent-300 cursor-pointer hover:bg-accent-250 dark:hover:bg-accent-750"
+          )}
+          <DistrictSearch value={district} setValue={setDistrict} />
+          <TextInput
+            value={username}
+            setValue={setUsername}
+            error={incorrectUsername}
+            label="Username"
+            placeholder="Enter your frontline username"
+          />
+          {changePassword ? (
+            <div className="flex flex-row w-full items-end gap-2">
+              <div className="w-full">
+                <TextInput
+                  value={password}
+                  setValue={setPassword}
+                  password={true}
+                  error={incorrectPassword}
+                  label="Password"
+                  placeholder="Your password will not be stored online"
+                />
+              </div>
+              {new URLSearchParams(window.location.search).get(
+                "changePassword"
+              ) == "false" && (
+                <div
+                  className="h-10 w-10 flex items-center justify-center bg-accent-150 dark:bg-accent-650 border border-accent-250 dark:border-accent-650 rounded-md text-accent-300 cursor-pointer hover:bg-accent-250 dark:hover:bg-accent-750"
+                  onClick={() => {
+                    setChangePassword(false);
+                  }}
+                >
+                  <IoClose />
+                </div>
+              )}
+            </div>
+          ) : (
+            <div>
+              <ChangeButton
                 onClick={() => {
-                  setChangePassword(false);
+                  setChangePassword(true);
                 }}
               >
-                <IoClose />
-              </div>
+                Password
+              </ChangeButton>
+              <p className="font-os">
+                You have a password stored on your device.
+              </p>
+            </div>
+          )}
+          <div className="mx-auto">
+            {checking ? (
+              <Loading />
+            ) : (
+              <button
+                disabled={!valid}
+                className={`py-2 px-4 rounded-md font-os ${
+                  valid
+                    ? "bg-accent-400 text-white"
+                    : "bg-mono-l-300 dark:bg-mono-d-300 text-mono-l-400 dark:text-mono-d-400 cursor-not-allowed"
+                }`}
+                onClick={submit}
+              >
+                Continue
+              </button>
             )}
           </div>
-        ) : (
-          <div>
-            <ChangeButton
-              onClick={() => {
-                setChangePassword(true);
-              }}
-            >
-              Password
-            </ChangeButton>
-            <p className="font-os">
-              You have a password stored on your device.
-            </p>
-          </div>
-        )}
-        <div className="mx-auto">
-          {checking ? (
-            <Loading />
-          ) : (
-            <button
-              disabled={!valid}
-              className={`py-2 px-4 from-accent-400 to-accent-500 rounded-md ${
-                valid
-                  ? "bg-gradient-to-tr text-white"
-                  : "bg-mono-l-300 dark:bg-mono-d-300 text-mono-l-400 dark:text-mono-d-400 cursor-not-allowed"
-              }`}
-              onClick={() => {
-                if (!valid) return;
-
-                setChecking(true);
-                assert(district != null);
-
-                props
-                  .checkSetup(
-                    district,
-                    username,
-                    changePassword ? password : undefined
-                  )
-                  .then((result) => {
-                    if (result === "VALID") {
-                      router.push("/app");
-                      return;
-                    }
-
-                    setChecking(false);
-
-                    if (result === "INCORRECT_PASSWORD") {
-                      setIncorrectPassword(true);
-                    }
-
-                    if (result === "INCORRECT_USERNAME") {
-                      setIncorrectUsername(true);
-                    }
-                  });
-              }}
-            >
-              Continue
-            </button>
-          )}
         </div>
-      </div>
+      </form>
     </FormPage>
   );
 }
