9e5b73267a930d442b2d7dcbcce89c3bedca0147
Adrian Casares <67245238+adriancasares@users.noreply.github.com>
2022-11-20 00:30:17 -0600

    add provideuninstallreason

--- pages/uninstall.tsx
+++ pages/uninstall.tsx
@@ -1,13 +1,20 @@
 import type { NextPage } from "next";
 import { NextSeo } from "next-seo";
-import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import Head from "next/head";
 import { useEffect, useState } from "react";
 import DeleteDataRow from "../components/app/uninstall/DeleteDataRow";
 import FormPage from "../components/core/FormPage";
-
+import { motion } from "framer-motion";
+import axios from "axios";
+import Loading from "../components/core/util/Loading";
+import {
+  IoCheckmarkCircle,
+  IoCloseCircle,
+  IoCloseCircleOutline,
+} from "react-icons/io5";
 export type DeletionStatus =
   | "SUCCESS"
   | "PENDING"
@@ -41,6 +48,35 @@ const Uninstall: NextPage = () => {
     }
   }, [router.query]);
 
+  const [uninstallReason, setUninstallReason] = useState<string | undefined>(
+    undefined
+  );
+
+  const [uninstallStatus, setUninstallStatus] = useState<
+    "LOADING" | "DONE" | "ERROR"
+  >("LOADING");
+
+  useEffect(() => {
+    if (uninstallReason) {
+      if (clientId) {
+        axios
+          .post("/api/metrics/provideUninstallReason", {})
+          .then((result) => {
+            if (result.data.success) {
+              setUninstallStatus("DONE");
+            } else {
+              setUninstallStatus("ERROR");
+            }
+          })
+          .catch((error) => {
+            setUninstallStatus("ERROR");
+          });
+      } else {
+        setUninstallStatus("ERROR");
+      }
+    }
+  }, [uninstallReason, clientId]);
+
   return (
     <>
       <NextSeo title="Extension Uninstalled" />
@@ -64,12 +100,54 @@ const Uninstall: NextPage = () => {
             Just One Question.
           </p>
           <p>Why did you uninstall Scorecard?</p>
-          <div className=" children:bg-mono-l-100 dark:bg-mono-d-100 children:py-4 children:px-6 children:border-b last:children:border-b-0 children:border-mono-l-300 dark:children:border-mono-d-300 rounded-md overflow-hidden shadow-sm font-os hover:children:bg-mono-l-150 children:transition-colors children:cursor-pointer">
-            <p>Too Complex to Use</p>
-            <p>Didn&apos;t Work Properly</p>
-            <p>Not What I&apos;m Looking For</p>
-            <p>No Longer Using Frontline</p>
-          </div>
+          {!uninstallReason && (
+            <div className=" group children:bg-mono-l-100 dark:children:bg-mono-d-100 children:border-b children:border-mono-l-300 dark:children:border-mono-d-300 rounded-md overflow-hidden shadow-sm font-os hover:children:bg-mono-l-150 dark:hover:children:bg-mono-d-150 children:transition-colors children:cursor-pointer">
+              {[
+                "Too Complex to Use",
+                "Didn't Work Properly",
+                "Not What I'm Looking For",
+                "No Longer Using Frontline",
+              ].map((reason, i) => (
+                <motion.p
+                  className="px-6 py-4"
+                  onClick={() => setUninstallReason(reason)}
+                  layoutId={`uninstall-reason-${reason}`}
+                  key={i}
+                >
+                  {reason}
+                </motion.p>
+              ))}
+            </div>
+          )}
+          {uninstallReason && (
+            <motion.div
+              initial={{
+                translateX: 20,
+              }}
+              layoutId={`uninstall-reason-${uninstallReason}`}
+              layout="position"
+            >
+              <div className=" bg-mono-l-100 dark:bg-mono-d-100 bg-gradient-to-b py-4 px-6 rounded-md overflow-hidden shadow-sm font-os flex justify-between items-center last:children:border-b-0">
+                <div className="flex-col gap-2">
+                  <p className="text-mono-l-600 dark:text-mono-d-600">
+                    Thanks for your feedback.
+                  </p>
+                  <p>It really helps us improve Scorecard.</p>
+                </div>
+                <div>
+                  {uninstallStatus === "LOADING" && <Loading />}
+
+                  {uninstallStatus === "DONE" && (
+                    <IoCheckmarkCircle className="text-accent-300 text-xl" />
+                  )}
+
+                  {uninstallStatus === "ERROR" && (
+                    <IoCloseCircle className="text-red-500 text-xl" />
+                  )}
+                </div>
+              </div>
+            </motion.div>
+          )}
         </div>
         <p className="text-sm max-w-md mx-auto">
           {clientId ? (
@@ -78,7 +156,7 @@ const Uninstall: NextPage = () => {
               linked linked to you. If you would like to delete your telemetry
               data,{" "}
               <a
-                className="underline"
+                className="underline cursor-pointer"
                 onClick={() => {
                   window.open(`/delete-telemetry-data?clientId=${clientId}`);
                 }}
