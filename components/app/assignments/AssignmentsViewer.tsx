3dc1360e3c79c310e696777a5e4ab657e652259c
Adrian Casares <67245238+adriancasares@users.noreply.github.com>
2022-11-20 21:34:35 -0600

    implement prefrences page

--- components/app/assignments/AssignmentsViewer.tsx
+++ components/app/assignments/AssignmentsViewer.tsx
@@ -1,9 +1,9 @@
 import React, { useContext, useEffect, useRef } from "react";
-import {FiRotateCw, FiX} from "react-icons/fi";
+import { FiRotateCw, FiX } from "react-icons/fi";
 import { motion } from "framer-motion";
 import { DataContext } from "scorecard-types";
 import GradeChip from "../GradeChip";
-import GradingCategorySelector from "../../core/input/GradingCategorySelector";
+import GradingCategorySelector from "../../core/input/Dropdown";
 import AssignmentCategory from "./AssignmentCategory";
 import CourseGradebook from "./CourseGradebook";
 import AssignmentsSidebar from "./AssignmentsSidebar";
@@ -30,21 +30,24 @@ export default function AssignmentsViewer(props: {
         if (e.target == e.view?.document.body) {
           if (e.key === "Escape") {
             setCourse(-1);
-          } else if (e.altKey && (e.key === "ArrowDown" || e.key === "ArrowUp")) {
+          } else if (
+            e.altKey &&
+            (e.key === "ArrowDown" || e.key === "ArrowUp")
+          ) {
             const courses = data.data?.courses?.length;
 
             if (courses) {
               if (e.key === "ArrowDown") {
-                setCourse(Math.min((course + 1), courses-1));
+                setCourse(Math.min(course + 1, courses - 1));
               } else {
-                setCourse(Math.max(0, course-1));
+                setCourse(Math.max(0, course - 1));
               }
 
               e.preventDefault();
             }
           }
         }
-      }
+      };
 
       document.addEventListener("click", handleClick, { capture: true });
       document.addEventListener("keydown", handleKey, { capture: true });
@@ -106,7 +109,15 @@ export default function AssignmentsViewer(props: {
                 <p></p>
               )}
             </div>
-            <button tabIndex={0} onClick={() => {setCourse(-1)}} className="absolute top-1 right-1 text-mono-l-500 hover:bg-slate-100 rounded-md p-1"><FiX/></button>
+            <button
+              tabIndex={0}
+              onClick={() => {
+                setCourse(-1);
+              }}
+              className="absolute top-1 right-1 text-mono-l-500 hover:bg-slate-100 rounded-md p-1"
+            >
+              <FiX />
+            </button>
           </div>
         </motion.div>
       </motion.div>
