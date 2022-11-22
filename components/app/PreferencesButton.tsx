92a0ac5e53f9e644ec4bba02e76e3f7981ad1eb0
Adrian Casares <67245238+adriancasares@users.noreply.github.com>
2022-11-22 02:38:41 -0600

    fix preferences button

--- components/app/PreferencesButton.tsx
+++ components/app/PreferencesButton.tsx
@@ -5,14 +5,12 @@ import { IoSettingsOutline } from "react-icons/io5";
 export default function PreferencesButton() {
   return (
     <Link href={"/app/preferences"}>
-      <a href="/app/preferences">
-        <div className="flex text-sm items-center gap-2 text-mono-l-500 dark:text-mono-d-500 group cursor-pointer">
-          <div className="group-hover:bg-accent-200 dark:group-hover:bg-accent-750 p-1 rounded-md text-mono-l-500 dark:text-mono-d-500">
-            <IoSettingsOutline />
-          </div>
-          <p>Preferences</p>
+      <div className="flex text-sm items-center gap-2 text-mono-l-500 dark:text-mono-d-500 group cursor-pointer">
+        <div className="group-hover:bg-accent-200 dark:group-hover:bg-accent-750 p-1 rounded-md text-mono-l-500 dark:text-mono-d-500">
+          <IoSettingsOutline />
         </div>
-      </a>
+        <p>Preferences</p>
+      </div>
     </Link>
   );
 }
