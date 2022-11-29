3c1c7b8cf85a0e31c3d5449c1c097924105e9b5d
MaliciousFiles <mtroalson@gmail.com>
2022-11-29 08:19:47 -0600

    editable grades

--- components/app/assignments/TableRow.tsx
+++ components/app/assignments/TableRow.tsx
@@ -4,6 +4,57 @@ import { Assignment } from "scorecard-types";
 export default function TableRow(props: { assignment: Assignment }) {
   const gradeRef = React.useRef<HTMLDivElement>(null);
 
+  const focusLost = (evt: React.FocusEvent<HTMLInputElement>) => {
+    const el = evt.currentTarget;
+
+    if (!el.value) {
+      el.value = el.defaultValue;
+
+      el.classList.remove("text-red-600");
+
+      return;
+    }
+
+    if (el.value.includes("/")) {
+      el.value = el.value.replace(/^\//, "0/");
+      if (el.value.match(/\/$/)) el.value = "0";
+
+      el.value = (eval(el.value)*100).toString();
+    }
+
+    el.value.replaceAll("%", "");
+    el.value = el.value.replace(/\.0?$/, "");
+    el.value = (Math.round(parseFloat(el.value)*10)/10).toString();
+    el.value += "%";
+
+    el.classList.add("text-red-600");
+  }
+
+  const filterInput = (evt: React.KeyboardEvent<HTMLInputElement>) => {
+    const el = evt.currentTarget;
+    const sel = el.selectionStart ?? 0;
+
+    if (evt.key === "Enter") el.blur();
+    if (evt.key.length > 1 || evt.altKey || evt.metaKey || evt.ctrlKey) return;
+
+    if (!evt.key.match(/[0-9.%\/]/)) evt.preventDefault();
+
+    if (evt.key === "/" && el.value.match(/[.%]/g)) evt.preventDefault();
+    if (evt.key.match(/[.%]/) && el.value.includes("/")) evt.preventDefault();
+    if (el.value.includes("%") && sel > el.value.indexOf("%")) evt.preventDefault();
+    if (evt.key === "." && el.value.includes(".")) evt.preventDefault();
+    if (evt.key.match(/[0-9]/)) {
+      if (el.value.includes("/")) {
+        if ((el.value.split("/")[!el.value.includes("/") || sel < el.value.indexOf("/") ? 0 : 1].match(/[0-9]/g)?.length ?? 0) >= 3) evt.preventDefault();
+      } else {
+        let idx = !el.value.includes(".") || sel < el.value.indexOf(".") ? 0 : 1;
+
+        if ((el.value.split(".")[idx].match(/[0-9]/g)?.length ?? 0) >= (idx || 3)) evt.preventDefault();
+      }
+    }
+
+  }
+
   return (
     <div className="text-sm pr-4 pt-1">
       <div className="flex items-center whitespace-nowrap justify-start">
@@ -55,9 +106,7 @@ export default function TableRow(props: { assignment: Assignment }) {
                 className="bg-mono-l-200 dark:bg-mono-d-200 py-1 px-2 rounded-sm"
                 ref={gradeRef}
               >
-                <p className="text-mono-l-600 dark:text-mono-d-600 text-center cursor-default">
-                  {props.assignment.grade}
-                </p>
+                <input onKeyDown={filterInput} onFocus={(evt: React.FocusEvent<HTMLInputElement>) => {evt.currentTarget.value = ""}} onBlur={focusLost} className="cursor-text bg-transparent w-full text-mono-l-600 dark:text-mono-d-600 text-center focus:text-red-600" defaultValue={props.assignment.grade} placeholder={props.assignment.grade} />
               </div>
             </div>
           </div>
