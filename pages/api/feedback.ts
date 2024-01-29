09bfd07516b3aad7eb61ef7ada8a713a3fbe5e4a
Adrian Casares <67245238+adriancasares@users.noreply.github.com>
2024-01-28 21:53:48 -0600

    add feedback api route

---
+++ pages/api/feedback.ts
@@ -0,0 +1,133 @@
+import dotenv from "dotenv";
+import { NextApiRequest, NextApiResponse } from "next";
+import admin from "firebase-admin";
+import { getFirestore } from "firebase-admin/firestore";
+import multiparty from "multiparty";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  dotenv.config();
+
+  const firebaseConfig = {
+    type: "service_account",
+    project_id: "scorecardto",
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY,
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_CLIENT_ID,
+    auth_uri: "https://accounts.google.com/o/oauth2/auth",
+    token_uri: "https://oauth2.googleapis.com/token",
+    auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+    client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+  };
+
+  const app = admin.apps.length
+    ? admin.app()
+    : admin.initializeApp({
+        // @ts-ignore
+        credential: admin.credential.cert(firebaseConfig),
+      });
+
+  console.log(req);
+
+  const db = getFirestore(app);
+
+  const {
+    reason,
+    firstName,
+    lastName,
+    message,
+    urgent,
+    distrct,
+    username,
+    password,
+    token,
+  } = req.body;
+
+  const decodedToken = await admin.auth().verifyIdToken(token);
+
+  if (reason !== "HELP" && reason !== "BUG" && reason !== "SUGGESTION") {
+    res.status(200).json({ success: false, error: "INVALID_REASON" });
+    return;
+  }
+
+  if (!firstName || firstName.length < 1) {
+    res.status(200).json({ success: false, error: "INVALID_FIRST_NAME" });
+    return;
+  }
+
+  if (firstName && firstName.length > 50) {
+    res.status(200).json({ success: false, error: "INVALID_FIRST_NAME" });
+    return;
+  }
+
+  if (lastName && lastName.length > 50) {
+    res.status(200).json({ success: false, error: "INVALID_LAST_NAME" });
+    return;
+  }
+
+  if (lastName && lastName.length > 50) {
+    res.status(200).json({ success: false, error: "INVALID_LAST_NAME" });
+    return;
+  }
+
+  if (!message || message.length < 1) {
+    res.status(200).json({ success: false, error: "INVALID_MESSAGE" });
+    return;
+  }
+  if (message && message.length > 5000) {
+    res.status(200).json({ success: false, error: "INVALID_MESSAGE" });
+    return;
+  }
+
+  if (distrct && distrct.length > 100) {
+    res.status(200).json({ success: false, error: "INVALID_DISTRICT" });
+    return;
+  }
+
+  if (username && username.length > 50) {
+    res.status(200).json({ success: false, error: "INVALID_USERNAME" });
+    return;
+  }
+
+  if (password && password.length > 50) {
+    res.status(200).json({ success: false, error: "INVALID_PASSWORD" });
+    return;
+  }
+
+  if (!username || !password || !distrct) {
+    const docRef = await db.collection("feedback").add({
+      reason,
+      name: {
+        firstName,
+        lastName,
+      },
+      urgent: urgent || false,
+    });
+
+    const doc = await docRef.get();
+
+    res.status(200).json({ success: true });
+  } else {
+    const docRef = await db.collection("feedback").add({
+      reason,
+      name: {
+        firstName,
+        lastName,
+      },
+      phoneNumber: decodedToken.phone_number || "NONE",
+      login: {
+        username,
+        password,
+        distrct,
+      },
+      urgent: urgent || false,
+    });
+
+    const doc = await docRef.get();
+
+    res.status(200).json({ success: true });
+  }
+}
